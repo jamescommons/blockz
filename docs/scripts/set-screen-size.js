222a01a3f0450381670154132fd6f5479415d609
jamescommons <jcommons2002@gmail.com>
2021-08-31 22:11:29 -0600

    Finished main menu layout

--- docs/scripts/set-screen-size.js
+++ docs/scripts/set-screen-size.js
@@ -4,8 +4,10 @@ let height = window.innerHeight;
 let resize = () => {
     document.querySelector('#main-menu-content')
             .setAttribute('style', `width: ${width}px; height: ${height}px;`);
+    width = window.innerWidth;
+    height = window.innerHeight;
 }
 
 resize();
 
-document.addEventListener('resize', resize);
+window.addEventListener('resize', resize);
